794353357e9132ad30a74edb79640448044f69fc
HomaX1 <baitasova.anastasiia@gmail.com>
2017-08-02 20:26:24 +0300

    started to do markup for second step

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(function () {
+    'use strict';
+
+    const ALLCONST = {
+        inputWrap: $('.user'),
+        input: $('.user__input-text'),
+        titleInput: $('.user__title'),
+        select: $('.type-wrap__title'),
+        insideOption: $('.type')
+    };
+
+    /*let inputActive = () => {
+        ALLCONST.titleInput.hide();
+        ALLCONST.inputWrap.find('.user__title').fadeIn();
+        ALLCONST.input.attr('placeholder', '');
+    };
+
+    ALLCONST.input.click(inputActive);*/
+
+});
